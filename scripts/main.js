5cb5ebcd4c0851f28fed174e9acb6eadb44ba106
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-05-10 16:52:52 +0300

    Upgrated styles(fixed #26)
    Gave transition property to the animation, fixed margins between elements, corrected variable names

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,12 @@
-function Listener() {
+function MouseOver() {
     var img = document.getElementById("picture");
     img.style.border="12px solid floralwhite";
     img.style.marginLeft = "-20px";
+    img.style.transition ="all .25s ease-in-out .05s";
 }
-
-
-function Lis2() {
+function MouseOut() {
     var img = document.getElementById("picture");
     img.style.border="7px solid floralwhite";
     img.style.marginLeft = "10px;";
-
+    img.style.transition ="all .13s linear";
 }
