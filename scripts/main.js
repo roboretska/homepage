93bc13ec545e1f6cb1220c391f0405bf19d657d9
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-05-10 16:29:35 +0300

    Made simple animation to picture(fixed #25 )

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function Listener() {
+    var img = document.getElementById("picture");
+    img.style.border="12px solid floralwhite";
+    img.style.marginLeft = "-20px";
+}
+
+
+function Lis2() {
+    var img = document.getElementById("picture");
+    img.style.border="7px solid floralwhite";
+    img.style.marginLeft = "10px;";
+
+}
